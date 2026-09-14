f0733ec15151d86f8b16b987bf0d26490938c2b6
Soulfrya <70708786+craterworldstudio@users.noreply.github.com>
2026-09-14 09:12:03 +0530

    Refactor boot.asm for better initialization
    Refactor boot.asm to improve initialization and add disk access parameters.

--- src/boot.asm
+++ src/boot.asm
@@ -1,22 +1,41 @@
 bits 16
 org 0x7C00
 
-cli
-cld
+start:
 
-xor ax, ax
-mov ds, ax
-mov es, ax
-mov ss, ax
-mov si, ax
-mov di, ax
-mov sp, 0x7C00
+  mov [boot_drive], dl
 
-std
+  cli
+  cld
+
+  xor ax, ax
+  mov ds, ax
+  mov es, ax
+  mov ss, ax
+  mov si, ax
+  mov di, ax
+  mov sp, 0x7C00
+
+
+  mov ah, 0x42
+  mov si, dap
+
+  int 13h
 
 
 
+dap:
+  db 10h
+  db 0
 
+  dw 4
+  dw 0x8000
+  dw 0x0000
+
+  dq 1
+
+
+boot_drive db 0
 
 jmp $
 
